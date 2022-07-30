a1a750888a637ae29750f78494965931f6b4353d
Ozodbekyum <hello7ozod@gamil.com>
2022-07-30 11:13:09 +0500

    if01

--- bin/if01.dart
+++ bin/if01.dart
@@ -8,5 +8,21 @@ Args:
 Returns:
     int: return answer.
 */
+int func(int a, int b, int c) {
+  int u = 0;
+  if (a > b) {
+    if (a > c) {
+      return a;
+    }
+  } else if (b > a) {
+    if (b > c) {
+      return b;
+    }
+  }
+
+  return c;
+}
+
 void main() {
+  print(func(115, 200, 198));
 }
