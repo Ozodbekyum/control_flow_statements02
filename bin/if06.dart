7f3f637efa502cb7f262e890e6304e4203e093da
Ozodbekyum <hello7ozod@gamil.com>
2022-07-30 12:22:09 +0500

    if06

--- bin/if06.dart
+++ bin/if06.dart
@@ -6,5 +6,25 @@ Args:
 Returns:
     int: return answer. 
 */
+int func(int n) {
+  int v1, v2, v3, v4, v5;
+  v1 = n % 10; //0
+  v2 = n ~/ 10 % 10; //7
+  v3 = n ~/ 10 ~/ 10 % 10; //6
+  v4 = n ~/ 10 ~/ 10 ~/ 10 ~/ 10 % 10; //4
+  v5 = n ~/ 10 ~/ 10 ~/ 10 ~/ 10 ~/ 10 % 10; //5
+  if (v1 > v2) {
+    return 1;
+  } else if (v2 > v3) {
+    return 2;
+  } else if (v3 > v4) {
+    return 3;
+  } else if (v4 > v5) {
+    return 4;
+  }
+  return 5;
+}
+
 void main() {
+  print(func(54670));
 }
