0631ab01836cf45719b6b582435ce18973cf488f
Ozodbekyum <hello7ozod@gamil.com>
2022-07-30 11:36:46 +0500

    if03

--- bin/if03.dart
+++ bin/if03.dart
@@ -6,6 +6,21 @@ Args:
     b: Second number.
     c: Third number.
 Returns:
-    int: return answer. */
+    int: return answer. 
+*/
+int func(int a, int b, int c) {
+  if (a > b) {
+    if (a > c) {
+      return a;
+    }
+  } else if (c > a) {
+    if (c < b) {
+      return c;
+    }
+  }
+  return b;
+}
+
 void main() {
+  print(func(9, 5, 10));
 }
